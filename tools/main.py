134dedce2364957b4984726cc19f1c0b4a4027c4
Jacob <jacob@britecore.com>
2025-07-03 16:36:31 -0500

    Clean up get_document_content

--- tools/main.py
+++ tools/main.py
@@ -100,6 +100,7 @@ class DocumentContent(BaseModel):
     document_id: str
     document_subsection_id: str
     content: str
+    document_subsections: DocumentSubsection
     created_at: str
     updated_at: Optional[str] = None
 
@@ -112,7 +113,7 @@ class Document(BaseModel):
     status: str
     created_at: str
     updated_at: Optional[str] = None
-    contents: Optional[List[DocumentContent]]
+    document_content: Optional[List[DocumentContent]]
 
 class DocumentContentItem(BaseModel):
     subsection_name: str
